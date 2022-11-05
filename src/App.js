76c4fbfa4e5f19ff74514b74840c47c7f7b271cd
Andrés Chávez <andreschavez@MacBook-Pro-de-Andres.local>
2022-11-05 16:35:42 -0600

    Primer commit, auntenticación API Spotify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+const CLIENT_ID = "544db08170194c919222022a5b35eac1"
+const REDIRECT_URI = "http://localhost:3000/"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Twicetify Login</h4>
+       <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+
     </div>
   );
 }
---
+++ src/Components/Buscador.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Buscador = () => {
+  return (
+    
+    <div>Buscador</div>
+  )
+}
+
+export default Buscador
