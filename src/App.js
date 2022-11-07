8362b2798ebc756640f851276a8f086b73353242
Andrés Chávez <andreschavez@MacBook-Pro-de-Andres.local>
2022-11-07 16:59:37 -0600

    Login mejorado, primeros pasos del buscador y bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
+import * as React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
+import '@fontsource/roboto'
+import Buscador from './Components/Buscador';
 const CLIENT_ID = "544db08170194c919222022a5b35eac1"
 const REDIRECT_URI = "http://localhost:3000/"
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
 const RESPONSE_TYPE = "token"
 
 
+
 function App() {
-  
-
   return (
-    <div className="App">
+    
+    <Container>
+         <br/>
       <h4>Twicetify Login</h4>
-       <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+      <Button variant="success" href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</Button>{' '}
+        <br/>
+          <br/>
+      <Buscador/>
 
-    </div>
+    </Container>
+    
+    
   );
 }
+
+
+
 
 export default App;
--- src/Components/Buscador.js
+++ src/Components/Buscador.js
@@ -1,10 +1,27 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
+import '@fontsource/roboto'
 const Buscador = () => {
-  return (
-    
-    <div>Buscador</div>
-  )
+    return (
+
+        <div className='Buscador'>
+            <Container>
+                <InputGroup className='mb-3' size='lg'>
+                    <FormControl
+                        placeholder='Ingresa el nombre de un artista'
+                        type='input'
+                        onKeyPress={ event => {
+                            if (event.key === "Enter");
+                            console.log("Presionaste enter"); 
+                        }}
+
+                    />
+                </InputGroup>
+
+            </Container>
+        </div>
+    )
 }
 
 export default Buscador
