10bfa9b17cde1e55eaca770ce8a1911d6025178f
Andrés Chávez <andreschavez@MacBook-Pro-de-Andres.local>
2022-11-07 21:30:04 -0600

    Fetch con los datos de autenticacion y primeros pasos para comenzar el llamado de la API

--- src/Components/Buscador.js
+++ src/Components/Buscador.js
@@ -1,46 +1,60 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, InputGroup, FormControl, Button, Row,} from 'react-bootstrap';
+import { Container, InputGroup, FormControl, Button, Row, } from 'react-bootstrap';
 import '@fontsource/roboto'
 import { useState, useEffect } from 'react';
+const CLIENT_ID = "544db08170194c919222022a5b35eac1"
+const CLIENT_SECRET = "414463376b2d4a8db5138eb97fb73a64"
+
 
 
 const Buscador = () => {
     const [searchInput, setSearchInput] = useState(""); //Aqui ponemos nuestro setter y nuestro getter con un estado inicial que es un string vacio
-    useEffect (() => {
-         
-    })
+    //API Access token
+    useEffect(() => {
+        var authParemeters = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+}
+fetch("http://accounts.spotify.com/api/token", authParemeters)   
+    .then(result => result.json())
+    .then(data => console.log(data))
+    }, [])
 
 
 
-    return (
+return (
+    <>
+    <div className='Buscador'>
+        <Container>
+            <InputGroup className='mb-3' size='lg'>
+                <FormControl
+                    placeholder='Ingresa el nombre de un artista'
+                    type='input'
+                    onKeyPress={event => {
+                        if (event.key === "Enter");
+                        console.log("Presionaste enter");
+                        //Aqui detecta con un clg que se envia un evento presionando enter o más abajo presionando el boton de buscar
+                    }}
+                    onChange={event => setSearchInput(event.target.value)}
 
-        <div className='Buscador'>
-            <Container>
-                <InputGroup className='mb-3' size='lg'>
-                    <FormControl
-                        placeholder='Ingresa el nombre de un artista'
-                        type='input'
-                        onKeyPress={ event => {
-                            if (event.key === "Enter");
-                            console.log("Presionaste enter"); 
-                            //Aqui detecta con un clg que se envia un evento presionando enter o más abajo presionando el boton de buscar
-                        }}
-                        onChange={event => setSearchInput(event.target.value)}
-
-                        />
-                        <Button onClick={() => {console.log("Presionaste el botón buscar")}}>
-                            Buscar
-                        </Button> 
-
-                            
+                />
+                <Button onClick={() => { console.log("Presionaste el botón buscar") }}>
+                    Buscar
+                </Button>
 
 
-                </InputGroup>
 
-            </Container>
-        </div>
-    )
+
+            </InputGroup>
+
+        </Container>
+    </div>
+    </>
+)
 }
 
 export default Buscador
